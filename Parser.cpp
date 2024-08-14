76f97c8435c77cf0981fb26fde35ba182c476e04
duckgii <shinyonggi@naver.com>
2024-08-14 18:18:44 +0900

    Feat:Parser Object

--- Parser.cpp
+++ Parser.cpp
@@ -1,11 +1,36 @@
 #include "Parser.hpp"
 
-Parser::Parser(const char * line)
-{
-	
-}
-
+Parser::Parser(std::string buf, int socket_fd) : _buf(buf), _socket_fd(socket_fd) {}
 Parser::~Parser()
 {
+}
+std::string Parser::get_command()
+{
+    std::string     input(_buf);
+    std::string     buffer;
+    std::string     line;
+    std::string     command;
+    std::istringstream   iss(input);
+    if (std::getline(iss, buffer))
+    {
+		buffer = buffer.substr(0, input.find("\r\n"));
+        _buf = _buf.substr(input.find("\r\n") + 2, input.size() - 1);
+        return (buffer);
+    }
+    else
+        return ("");
+}
 
+std::vector<std::string>    Parser::split_command(std::string command)
+{
+    std::string     input(command);
+    std::string     buffer;
+    std::istringstream   iss(input);
+    std::vector<std::string> ret;
+    while (std::getline(iss, buffer))
+    {
+		buffer = buffer.substr(0, input.find("\r\n"));
+        ret.push_back(buffer);
+    }
+    return (ret);
 }
--- Parser.hpp
+++ Parser.hpp
@@ -1,14 +1,19 @@
 #ifndef PARSER_HPP
 # define PARSER_HPP
-# include <iostream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
 
 class Parser
 {
-	public:
-		Parser(const char * line);
-		~Parser();
-
-		
+    public:
+        Parser(std::string buf, int socket_fd);
+        ~Parser();
+        std::string get_command();
+        std::vector<std::string>    split_command(std::string command);
+    private:
+        std::string _buf;
+        int         _socket_fd;
 };
-
 #endif
