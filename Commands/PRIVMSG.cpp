ebadfbc98a4f85a06853f8f01caa8bff1f9bd994
JeongTJ <dmswl022329@gmail.com>
2024-08-20 13:11:14 +0900

    feat : 유저 대상 메시지 전송기능 추가, 채널대상 메서드 필요 #30

---
+++ Commands/PRIVMSG.cpp
@@ -0,0 +1,64 @@
+#include "PRIVMSG.hpp"
+#include <cctype>
+#include <sstream>
+
+PRIVMSG::PRIVMSG() {
+	
+}
+
+PRIVMSG::PRIVMSG(const PRIVMSG& other): Command(other) {
+
+}
+
+PRIVMSG& PRIVMSG::operator=(const PRIVMSG& other) {
+	if (this == &other)
+		return *this;
+	Command::operator=(other);
+	return *this;
+}
+
+PRIVMSG::~PRIVMSG() {
+
+}
+
+void PRIVMSG::execute(Server& server, Client& client) {
+	if (this->_cmdSource.size() < 3) {
+		client.send(makeNumericMsg(server, client, ERR_NEEDMOREPARAMS));
+		return ;
+	}
+	fillSendTarget();
+	for (vector<string>::iterator it = this->sendTarget.begin(); it != this->sendTarget.end(); it++) {
+		this->_cmdSource[1] = *it;
+		if (this->_cmdSource[1][0] != '#') {
+			Client* targetClient = server.getClient(this->_cmdSource[1]);
+			if (targetClient == NULL) {
+				client.send(makeNumericMsg(server, client, this->_cmdSource[1], ERR_NOSUCHNICK));
+			} else {
+				targetClient->send(":" + client.getNickName() + "!~" + client.getUserName() + "@" + client.getHostName() + " " + this->_cmdSource[0] + " " + this->_cmdSource[1] + " " + this->_cmdSource[2] + "\r\n");
+				// targetClient->send(":" + client.getNickName() + "!~" + client.getUserName() + "@" + client.getHostName() + " " + this->_cmdSource[0] + " " + this->_cmdSource[1] + " " + this->_cmdSource[2]);
+			}
+		} else if (this->_cmdSource[1][0] == '#') {
+			Channel* targetChannel = server.getChannel(this->_cmdSource[1]);
+			if (targetChannel == NULL) {
+				client.send(makeNumericMsg(server, client, this->_cmdSource[1], ERR_CANNOTSENDTOCHAN));
+			} else {
+				// targetChannel->send(":" + client.getNickName() + "!~" + client.getUserName() + "@" + client.getHostName() + " " + this->_cmdSource[0] + " " + this->_cmdSource[1] + " " + this->_cmdSource[2] + "\r\n");
+			}
+		}
+	}
+	this->sendTarget.clear();
+}
+
+void PRIVMSG::fillSendTarget() {
+	string targets = this->_cmdSource[1];
+	for (string::iterator it = targets.begin(); it != targets.end(); it++)
+		if (*it == ',')
+			*it = ' ';
+	istringstream iss(targets);
+	string tmp;
+	while (!iss.eof()) {
+		iss >> tmp;
+		if (tmp != "")
+			this->sendTarget.push_back(tmp);
+	}
+}
