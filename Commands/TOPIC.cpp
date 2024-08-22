83fe6d3054402940b942171b2af54f464e997de4
Choi Yeondeok <yeondeok99@naver.com>
2024-08-22 14:27:19 +0900

    feat: 채널 topic 설정시 채널 옵션 및 관리자 권한 확인 로직 추가

--- Commands/TOPIC.cpp
+++ Commands/TOPIC.cpp
@@ -8,21 +8,26 @@ void	TOPIC::execute(Server& server, Client& client)
 {
 	string msg;
 	Channel* ch = server.getChannel(_cmdSource[1]);
-	if (_cmdSource.size() == 2)
-		msg = ch->getName() + " topic is " + ch->getTopic() + ".\r\n"; 
-	else
+	if (ch->getMode(ch->TOPIC_OPER_ONLY) && ch->isOper(&client))
 	{
-		if (_cmdSource[2].length() == 1)
-		{
-			ch->clearTopic();
-			msg = ch->getName() + " topic is cleared\r\n.";
-		}
+		if (_cmdSource.size() == 2)
+			msg = ch->getName() + " topic is " + ch->getTopic() + ".\r\n"; 
 		else
 		{
-			string topic = _cmdSource[2].erase(0);
-			ch->setTopic(topic);
-			msg = ch->getName() + " topic is changed to " + topic + ".\r\n";
+			if (_cmdSource[2].length() == 1)
+			{
+				ch->clearTopic();
+				msg = ch->getName() + " topic is cleared\r\n.";
+			}
+			else
+			{
+				string topic = _cmdSource[2].erase(0);
+				ch->setTopic(topic);
+				msg = ch->getName() + " topic is changed to " + topic + ".\r\n";
+			}
 		}
 	}
+	else
+		msg = makeNumericMsg(server, client, "482");
 	client.send(msg);
 }
