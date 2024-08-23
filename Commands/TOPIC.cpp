77dbf7da593073e5a2d8dac98f40dde33576269b
Choi Yeondeok <yeondeok99@naver.com>
2024-08-23 12:35:35 +0900

    fix: 여러가지 예외사항들 수정

--- Commands/TOPIC.cpp
+++ Commands/TOPIC.cpp
@@ -1,33 +1,62 @@
 #include "TOPIC.hpp"
 
+#include <chrono>
+#include <iostream>
+
 TOPIC::TOPIC() {}
 TOPIC::TOPIC(const TOPIC& ref) { *this = ref; }
 TOPIC::~TOPIC() {}
 
 void	TOPIC::execute(Server& server, Client& client)
 {
-	string msg;
+	string msg = ":" + server.getServername() + " ";
 	Channel* ch = server.getChannel(_cmdSource[1]);
+	if (ch == NULL)
+	{
+		client.send(makeNumericMsg(server, client, _cmdSource[1], "403"));
+		return ;
+	}
+	if (_cmdSource.size() < 2)
+	{
+		client.send(makeNumericMsg(server, client, "461"));
+		return ;
+	}
+	if (!ch->checkUserInChannel(client.getNickName()))
+	{
+		client.send(makeNumericMsg(server, client, ch->getName(), "442"));
+		return;
+	}
 	if ((ch->getMode(ch->TOPIC_OPER_ONLY) && ch->isOper(&client)) || !ch->getMode(ch->TOPIC_OPER_ONLY))
 	{
 		if (_cmdSource.size() == 2)
-			msg = ch->getName() + " topic is " + ch->getTopic() + ".\r\n"; 
+		{
+			if (ch->getTopic().length() == 0)
+				msg += "331 " + client.getNickName() + " " + ch->getName() + " :No topic is set\r\n";
+			else
+				msg += "332 " + client.getNickName() + " " + ch->getName() + " :" + ch->getTopic() + "\r\n";
+		}
 		else
 		{
 			if (_cmdSource[2].length() == 1)
 			{
 				ch->clearTopic();
-				msg = ch->getName() + " topic is cleared\r\n.";
+				msg += "332 " + client.getNickName() + " " + ch->getName() + "\r\n";
 			}
 			else
 			{
-				string topic = _cmdSource[2].erase(0);
+				string topic = _cmdSource[2].erase(0, 1);
 				ch->setTopic(topic);
-				msg = ch->getName() + " topic is changed to " + topic + ".\r\n";
+				msg += "332 " + client.getNickName() + " " + ch->getName() + " :" + ch->getTopic() + "\r\n";
+			}
+
+			vector<Client*> users = ch->getUsers();
+			for (size_t i = 0; i < users.size(); ++i)
+			{
+				users[i]->send(msg);
+				users[i]->send(":" + server.getServername() + " 333 " + users[i]->getNickName() + " " + ch->getName() + " " + client.getNickName() + " " + to_string(chrono::duration_cast<chrono::seconds>(chrono::system_clock::now().time_since_epoch()).count()) + "\r\n");
 			}
 		}
 	}
 	else
-		msg = makeNumericMsg(server, client, "482");
-	client.send(msg);
+		client.send(makeNumericMsg(server, client, ch->getName(), "482"));
 }
