e110a52097e96e6f18688b4ef19dbcc230f23aa5
Yeonggi Shin <yeoshin@c2r3s7.42seoul.kr>
2024-08-30 21:19:03 +0900

    fix : add bot

--- Server.cpp
+++ Server.cpp
@@ -37,6 +37,7 @@ void	Server::run()
 
 	if ((kq = kqueue()) == -1)
 		handle_error("kqueue error");
+	this->_clients.push_back(new Bot(this));
 	//bot client add
 	change_events(_change_list, _socket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
 	//debugging
